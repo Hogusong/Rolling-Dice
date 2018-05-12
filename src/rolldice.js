06e24530cb8026a618019ad1a4a5a86448756862
Young <hogusong1@gmail.com>
2018-05-11 22:35:44 -0700

    make rules for this game. save it to rolldice.js

---
+++ src/rolldice.js
@@ -0,0 +1,26 @@
+/*
+GAME RULES:
+
+-   The game has 2 players, playing in rounds.
+
+-   The players set the target score to start game(50 is default).
+
+-   In each turn, a player rolls a dice as many times as he whishes.
+    Each result get added to his ROUND score (current score).
+
+-   BUT, 
+    if the player rolls a 1, all his ROUND score gets lost. After that,
+    it's the next player's turn.
+
+    If the player rolls the same number twice in a row, all his/her total score
+    After that, it's the next player's turn.
+
+    If the player rolls two 6s, he/she will lose the game.           
+
+-   The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score.
+    After that, it's the next player's turn
+
+-   The first player to reach target points on GLOBAL score wins the game
+
+-   The player can choose 'New Game' to restart a New Game.
+*/
