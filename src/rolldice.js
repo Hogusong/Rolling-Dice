1ce64d737b997cd9bae43c0c40720eb2d4f91fd5
Young <hogusong1@gmail.com>
2018-05-12 16:37:33 -0700

    finish an EventListener for 'NEW GAME' modal in rolldice.js

--- src/rolldice.js
+++ src/rolldice.js
@@ -24,3 +24,67 @@ GAME RULES:
 
 -   The player can choose 'New Game' to restart a New Game.
 */
+
+var scores, roundScore, preDice, activePlayer, currentScore, gameOver;
+var winScore = 30;
+var lastWinScore = winScore;
+var DOM = {
+  dice: document.getElementById('dice'),
+  message: document.querySelector('.message'),
+  target: document.getElementById('target-score'),
+}
+
+init();
+
+function init() {
+  scores = [null, 0, 0];
+  roundScore = 0;
+  preDice = 0;
+  activePlayer = 1;
+  currentScore = 0;
+  gameOver = false;
+
+  DOM.dice.style.display = 'none';
+  DOM.message.style.display = 'none';
+  DOM.target.innerHTML = "Target : " + winScore;
+
+  document.getElementById('player-1').innerHTML = 'Player 1';
+  document.getElementById('total-score-1').textContent = '0';
+  document.getElementById('current-score-1').textContent = '0';
+  document.getElementById('player-2').innerHTML = 'Player 2';
+  document.getElementById('total-score-2').innerHTML = '0';
+  document.getElementById('current-score-2').innerText = '0';
+
+  document.querySelector('.player-1').classList.add('active');
+  document.querySelector('.player-2').classList.remove('active');
+}
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+  if (gameOver) {
+    DOM.message.style.display = 'block'
+    return
+  }
+})
+
+document.querySelector('.btn-hold').addEventListener('click', () => {
+  if (gameOver) {
+    DOM.message.style.display = 'block'
+    return
+  }
+})
+
+document.querySelector('.btn-new').addEventListener('click', ()=> {
+  document.querySelector('.modal').style.display = 'block';
+} )
+
+document.getElementById('cancel').addEventListener('click', () => {
+  document.querySelector('.modal').style.display = 'none';  
+})
+
+document.getElementById('submit').addEventListener('click', () => {
+  document.querySelector('.modal').style.display = 'none';  
+  const inputValue = document.getElementsByTagName('input')[0].value;
+  winScore = (parseInt(inputValue) > 20) ? parseInt(inputValue) : lastWinScore ;
+  lastWinScore = winScore;
+  init();
+})
